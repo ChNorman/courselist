0611fc11a39190f9d21441ce8eaee9de479b64e2
chris Norman <chnorman@microsoft.com>
2020-05-21 17:18:07 -0500

    add startup file

---
+++ src/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Enter the source directory to make sure the script runs where the user expects
+cd "/home/site/wwwroot"
+
+export NODE_PATH=$(npm root --quiet -g):$NODE_PATH
+if [ -z "$PORT" ]; then
+		export PORT=8080
+fi
+
+export NODE_OPTIONS='--require ./oryx-appinsightsloader.js ' $NODE_OPTIONS
+pm2 start --no-daemon /home/site/wwwroot
